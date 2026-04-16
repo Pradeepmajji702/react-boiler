d9d7f2dcc88eac2dac587a3b2c66a5a82dd732ed
Pradeepmajji702 <Pradeepmajji702>
2026-04-16 18:26:32 +0530

    refactor: restructure project and remove Redux store
    - Changed project name in package.json from "react-ts-tailwind-primereact" to "my-app".
    - Simplified build scripts in package.json by removing environment-specific builds.
    - Updated dependencies and devDependencies to their latest versions.
    - Removed unused files: postcss.config.cjs, web.xml, api.ts, index.ts, dataSlice.ts, and tailwind.config.cjs.
    - Refactored App component to use local state instead of Redux for managing count.
    - Updated main.tsx to remove Redux Provider.
    - Replaced App.css with Tailwind CSS imports in index.css.
    - Added new postcss.config.js and tailwind.config.js files for Tailwind CSS configuration.
    - Cleaned up styles and removed unused CSS variables.

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  envDir: 'environments',
   plugins: [react()],
-  base: '/react-deploy/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@fonts': path.resolve(__dirname, './src/assets/fonts'),
-      '@styles': path.resolve(__dirname, './src/assets/styles'),
-    },
-  },
 })
